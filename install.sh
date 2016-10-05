eed1e7f5df5d4fcb3e93530e2db6b2d02bfdcbff
Yugo Yagita <2000ygkn0713@gmail.com>
2016-10-05 22:25:56 +0900

    [add] add instal.sh

---
+++ install.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+ppas=$(cat) << EOS
+ppa:agornostal/ulauncher
+ppa:neovim-ppa/unstable
+ppa:japaneseteam/ppa
+EOS
+apps=$(cat) << EOS
+vlc
+shutter
+nemo
+launchy
+software-properties-common
+python-software-properties
+gnome-sushi
+openshot
+gimp
+inkscape
+ubuntu-restricted-extras
+git
+ibus-mozc
+xbacklight
+vim
+unity-tweak-tool
+zsh
+tmux
+leafpad
+nautilus-emblems
+python-dev
+python-pip
+python3-dev
+python3-pip
+xsel
+build-essential
+libssl-dev
+neovim
+ulauncher
+EOS
+downloadurls=$(cat) << EOS
+http://hluk.github.io/CopyQ/
+https://www.google.co.jp/chrome/browser/desktop/index.html
+EOS
+
+
+exeRoot(){
+  for url in $downloadurls; do
+    xdg-open $urls
+  done
+
+# Install
+  apt update
+  apt upgrade
+
+  for ppa in $ppas; do
+    add-apt-repository $ppa
+  done
+
+  apt update
+
+  for app in $apps; do
+    apt install $app
+  done
+
+
+# Node.js
+  apt install nodejs npm
+  npm cache clean
+  npm install n -g
+  n stable
+  ln -sf /usr/local/bin/node /usr/bin/node
+  node -v
+  apt purge -y nodejs npm
+}
+
+sudo exeRoot
+
+#Ruby rbenv
+cd
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# NeoVim
+update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+update-alternatives --config vi
+update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+update-alternatives --config vim
+update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+update-alternatives --config editor
+
+pip3 install neovim
+
+# commands
+LANG=C xdg-user-dirs-gtk-update
+/usr/share/doc/libdvdread4/install-css.sh
+
+# dotfiles
+git clone git@github.com:ygkn/dotfiles.git
+cd dotfiles
+ln -s $(pwd)/.* ~
+ln -s $(pwd)/nvim ~/.config/nvim
+
